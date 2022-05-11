ddce9e82eb219462e77156996aa65b11b7eb98cc
Arnaud Le Fur <afur@princeton.astro.edu>
2022-05-10 19:51:38 -1000

    testing with testsAtor.convergenceId key

--- python/pfsPlotActor/plots/convergenceMap.py
+++ python/pfsPlotActor/plots/convergenceMap.py
@@ -1,62 +1,44 @@
-
-import pfsPlotActor.livePlot as livePlot
-from ics.cobraCharmer import pfiDesign
-from ics.cobraCharmer import func
 import numpy as np
-from pfs.utils.butler import Butler
+import pfsPlotActor.livePlot as livePlot
+from ics.cobraCharmer import func
 from opdb import opdb
-import matplotlib.pylab as plt
-import pandas as pd
-import os
+from pfs.utils.butler import Butler
+
+
+def getCobraStatusIndex(calibModel):
+    """Retrieve good/bad cobra index."""
+    # get goodIdx
+    allCobras = [func.Cobra(calibModel.moduleIds[i], calibModel.positionerIds[i]) for i in calibModel.findAllCobras()]
+    nCobras = len(allCobras)
+
+    goodNums = [i + 1 for i, c in enumerate(allCobras) if calibModel.cobraIsGood(c.cobraNum, c.module)]
+    badNums = [e for e in range(1, nCobras + 1) if e not in goodNums]
+
+    goodIdx = np.array(goodNums, dtype='i4') - 1
+    badIdx = np.array(badNums, dtype='i4') - 1
+
+    return goodIdx, badIdx
+
 
 class ConvergenceMap(livePlot.LivePlot):
-    
     key = 'convergenceId'
     # needs to be overridden by the user.
-    actor = 'fps'
+    actor = 'tests'
 
     # load pfi design file
-    db = opdb.OpDB(hostname="localhost",username="karr",dbname="opdb")
+    db = opdb.OpDB(hostname="db-ics", username="pfs", dbname="opdb")
+    butler = Butler()
 
-    def __init__(self,xmlFile,dotFile):
+    dots = butler.get('black_dots')
+    fids = butler.get('fiducials')
+    calibModel = butler.get('moduleXml', moduleName='ALL', version='')
 
-        """calibration data only needs to be loaded once"""
-
-        self.calibModel = pfiDesign.PFIDesign(xmlFile)
-        self.dots = pd.read_csv(dotFile)
-
-        butler = Butler(configRoot=os.path.join(os.environ["PFS_INSTDATA_DIR"], "data"))
-        self.fids = butler.get('fiducials')
-        
-        #define goodIdx
-        cobras = []
-
-        #get goodIdx 
-        for i in self.calibModel.findAllCobras():
-            c = func.Cobra(self.calibModel.moduleIds[i],
-                           self.calibModel.positionerIds[i])
-            cobras.append(c)
-        allCobras = np.array(cobras)
-        nCobras = len(allCobras)
-        
-        goodNums = [i+1 for i,c in enumerate(allCobras) if
-                self.calibModel.cobraIsGood(c.cobraNum, c.module)]
-        badNums = [e for e in range(1, nCobras+1) if e not in goodNums]
-
-        self.goodIdx = np.array(goodNums, dtype='i4') - 1
-        self.badIdx = np.array(badNums, dtype='i4') - 1
-
-        
-    def initialize(self):
-        """Initialize my an axis to plot the map"""
-
-        self.ax = self.fig.add_subplot(111)
-        return self.ax
+    goodIdx, badIdx = getCobraStatusIndex(calibModel)
 
     def identify(self, keyvar):
 
         """load the convergence data"""
-        
+
         [visit] = keyvar.getValue()
         return self.loadConvergence(visit)
 
@@ -69,7 +51,12 @@ class ConvergenceMap(livePlot.LivePlot):
         """
         visitId = int(visitId)
 
-        sql = f'select cm.pfs_visit_id, cm.iteration, cm.cobra_id, cm.pfi_center_x_mm, cm.pfi_center_y_mm, ct.pfi_target_x_mm, ct.pfi_target_y_mm, md.mcs_center_x_pix, md.mcs_center_y_pix, md.mcs_second_moment_x_pix,md.mcs_second_moment_y_pix, md.peakvalue  from cobra_match cm inner join cobra_target ct on ct.pfs_visit_id = cm.pfs_visit_id and ct.iteration = cm.iteration and ct.cobra_id = cm.cobra_id inner join mcs_data md on md.mcs_frame_id = cm.pfs_visit_id * 100 + cm.iteration and md.spot_id = cm.spot_id where cm.pfs_visit_id = {visitId} order by ct.cobra_id, ct.iteration'
+        sql = f'select cm.pfs_visit_id, cm.iteration, cm.cobra_id, cm.pfi_center_x_mm, cm.pfi_center_y_mm, ' \
+              f'ct.pfi_target_x_mm, ct.pfi_target_y_mm, md.mcs_center_x_pix, md.mcs_center_y_pix, ' \
+              f'md.mcs_second_moment_x_pix,md.mcs_second_moment_y_pix, md.peakvalue  from cobra_match ' \
+              f'cm inner join cobra_target ct on ct.pfs_visit_id = cm.pfs_visit_id and ct.iteration = ' \
+              f'cm.iteration and ct.cobra_id = cm.cobra_id inner join mcs_data md ' \
+              f'on md.mcs_frame_id = cm.pfs_visit_id * 100 + cm.iteration and md.spot_id = cm.spot_id where cm.pfs_visit_id = {visitId} order by ct.cobra_id, ct.iteration'
 
         # get data
         convergeData = self.db.fetch_query(sql)
@@ -80,12 +67,12 @@ class ConvergenceMap(livePlot.LivePlot):
         except:
             nIter = 0
 
-        return dict(visitId=visitId, convergeData=convergeData, nIter=nIter)
+        return dict(visitId=visitId, convergeData=convergeData, iterVal=nIter)
 
     def plot(self, visitId, convergeData, iterVal):
-        
+
         """Plot the latest dataset."""
-        
+
         fig = self.fig
         ax = self.ax
 
