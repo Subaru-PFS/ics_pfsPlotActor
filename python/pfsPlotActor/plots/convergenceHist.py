6d4424a02391b5f0d6c4d3ca27b4f334ac966809
Arnaud Le Fur <afur@princeton.astro.edu>
2022-06-21 01:33:06 -1000

    add widgets for histogram(vmin,vmax,bins)

--- python/pfsPlotActor/plots/convergenceHist.py
+++ python/pfsPlotActor/plots/convergenceHist.py
@@ -8,7 +8,7 @@ reload(pfiUtils)
 
 class ConvergenceHist(pfiUtils.ConvergencePlot):
 
-    def plot(self, convergeData, bins=None):
+    def plot(self, convergeData, vmin=0, vmax=0.1, bins=20, minIter=4):
         """Plot the latest dataset."""
         fig = self.fig
         ax = self.axes[0]
@@ -16,19 +16,19 @@ class ConvergenceHist(pfiUtils.ConvergencePlot):
         nIter = convergeData.iteration.max()
 
         for iterVal in range(nIter):
+            if iterVal < minIter - 1:
+                continue
+
             iterData = convergeData.query(f'iteration=={iterVal}')
             dx = iterData.pfi_center_x_mm - iterData.pfi_target_x_mm
             dy = iterData.pfi_center_y_mm - iterData.pfi_target_y_mm
             dist = np.hypot(dx, dy)
 
-            if(bins == None):
-                n, bins, patches = ax.hist(dist, alpha=0.7, histtype='step', linewidth=3,
-                                           label=f'{iterVal + 1}-th Iteration')
-            else:
-                n, bins, patches = ax.hist(dist, alpha=0.7, histtype='step', linewidth=3,
-                                           label=f'{iterVal + 1}-th Iteration', bins=bins)
+            n, bins, patches = ax.hist(dist, alpha=0.7, histtype='step', linewidth=3,
+                                       label=f'{iterVal + 1}-th Iteration', bins=bins, range=(vmin, vmax))
+
             ax.legend(loc='upper right')
-            ax.set_title("Distance to Target: pfsVisitId = {visitId:d}")
+            ax.set_title(f"Distance to Target: pfsVisitId = {visitId:d}")
             ax.set_xlabel("Distance (mm)")
             ax.set_ylabel("N")
             ax.set_aspect('auto')
